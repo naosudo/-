821b7793419563d76cf1172d3f2655745e50b107
naosudo <nao@nicoriwa.com>
2025-08-15 04:38:46 +0900

    商品詳細表示機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
-  resources :items, only: [:index, :new, :create]
+  resources :items, only: [:index, :new, :create, :show] 
   
   root to: "items#index"
   
